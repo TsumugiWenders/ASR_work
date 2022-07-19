2b896e2aafbe026e6b120361808d2df4ff049451
baixf-xyz <707401057@qq.com>
2022-07-19 09:54:38 +0800

    report

--- 03-GMM-EM/gmm_estimator.py
+++ 03-GMM-EM/gmm_estimator.py
@@ -6,20 +6,22 @@ import scipy.cluster.vq as vq
 
 num_gaussian = 5
 num_iterations = 5
-targets = ['Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'O']
+
 
 class GMM:
+    # 1.GMM的初始化方法
     def __init__(self, D, K=5):
         assert(D>0)
-        self.dim = D
-        self.K = K
+        self.dim = D #MFCC 39维数据特征
+        self.K = K #要形成的簇数以及要生成的质心数,即num_gaussian
         #Kmeans Initial
         self.mu , self.sigma , self.pi = self.kmeans_initial()
 
+    # 2.使用kmeans聚类方法获得初始化参数
     def kmeans_initial(self):
         mu = []
         sigma = []
-        data = read_all_data('train/feats.scp')
+        data = read_all_data('train/feats.scp') # 句子id到特征数据真实路径和位置的映射
         (centroids, labels) = vq.kmeans2(data, self.K, minit="points", iter=100)
         clusters = [[] for i in range(self.K)]
         for (l,d) in zip(labels,data):
@@ -28,9 +30,9 @@ class GMM:
         for cluster in clusters:
             mu.append(np.mean(cluster, axis=0))
             sigma.append(np.cov(cluster, rowvar=0))
-        pi = np.array([len(c)*1.0 / len(data) for c in clusters])
+        pi = np.array([len(c)*1.0 / len(data) for c in clusters]) #每一类占总体的比重
         return mu , sigma , pi
-    
+    # 3.计算高斯混合分布 p10
     def gaussian(self , x , mu , sigma):
         """Calculate gaussion probability.
     
@@ -46,7 +48,7 @@ class GMM:
         mahalanobis = np.dot(mahalanobis, (x-mu))
         const = 1/((2*np.pi)**(D/2))
         return const * (det_sigma)**(-0.5) * np.exp(-0.5 * mahalanobis)
-    
+    # 4.计算对数似然函数 p12
     def calc_log_likelihood(self, X):
         """Calculate log likelihood of GMM
             param: X: A matrix including data samples, num_samples * D
@@ -63,6 +65,7 @@ class GMM:
         ) for n in range(N)])
         return log_llh
 
+    # 5.EM算法实现 p13
     def em_estimator(self, X):
         """Update paramters of GMM
             param: X: A matrix including data samples, num_samples * D
@@ -104,8 +107,8 @@ def train(gmms, num_iterations = num_iterations):
     dict_utt2feat, dict_target2utt = read_feats_and_targets('train/feats.scp', 'train/text')
     
     for target in targets:
-        feats = get_feats(target, dict_utt2feat, dict_target2utt)   #
-        for i in range(num_iterations):
+        feats = get_feats(target, dict_utt2feat, dict_target2utt)
+        for i in range(num_iterations): #训练迭代次数：5
             log_llh = gmms[target].em_estimator(feats)
     return gmms
 
@@ -132,12 +135,16 @@ def test(gmms):
     acc = correction_num * 1.0 / (correction_num + error_num)
     return acc
 
+targets = ['Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'O']
 
 def main():
     gmms = {}
+    # 1.为每一个字符创建一个GMM模型，保存在gmms字典中
     for target in targets:
-        gmms[target] = GMM(39, K=num_gaussian) #Initial model
+        gmms[target] = GMM(39, K=num_gaussian) #39维mfcc特征
+    # 2.在训练集上训练gmms中的混合高斯模型
     gmms = train(gmms)
+    # 3.在测试集上测试模型
     acc = test(gmms)
     print('Recognition accuracy: %f' % acc)
     fid = open('acc.txt', 'w')
