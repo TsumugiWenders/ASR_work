75bda96e2fc51a7471737b2b1ef935faed6f29ad
baixf-xyz <707401057@qq.com>
2022-08-11 23:11:58 +0800

    0811

--- 06-DNN-HMM/dnn.py
+++ 06-DNN-HMM/dnn.py
@@ -1,15 +1,26 @@
 # Author: Sining Sun, Zhanheng Yang, Binbin Zhang
-
+import math
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 import kaldi_io
 from utils import *
-
+ 
+def plot_spectrogram(spec, file_name):
+    fig = plt.figure(figsize=(20, 10))
+    plt.plot(spec)
+    plt.xlabel('epochs')
+    plt.ylabel('loss')
+    plt.savefig(file_name)
+    plt.show()
+ 
+ 
 targets_list = ['Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'O']
 targets_mapping = {}
 for i, x in enumerate(targets_list):
     targets_mapping[x] = i
-
-
+ 
+ 
 class Layer:
     def forward(self, input):
         ''' Forward function by input
@@ -19,7 +30,7 @@ class Layer:
             output when applied this layer
         '''
         raise 'Not implement error'
-
+ 
     def backward(self, input, output, d_output):
         ''' Compute gradient of this layer's input by (input, output, d_output)
             as well as compute the gradient of the parameter of this layer
@@ -32,80 +43,87 @@ class Layer:
             accumulated gradient from final output to this layer's input
         '''
         raise 'Not implement error'
-
+ 
     def set_learning_rate(self, lr):
         ''' Set learning rate of this layer'''
         self.learning_rate = lr
-
+ 
     def update(self):
         ''' Update this layers parameter if it has or do nothing
         '''
-
-
+ 
+ 
 class ReLU(Layer):
     def forward(self, input):
-        # BEGIN_LAB
-        # END_LAB
-
+        mat = np.maximum(0, input)
+        return mat.T
+ 
     def backward(self, input, output, d_output):
-        # BEGIN_LAB
-        # END_LAB
-
-
+        mat = np.array(d_output, copy=True)
+        mat[input <= 0] = 0
+        return mat.T
+ 
 class FullyConnect(Layer):
     def __init__(self, in_dim, out_dim):
-        self.w = np.random.randn(out_dim, in_dim) * np.sqrt(2.0 / in_dim)
-        self.b = np.zeros(out_dim)
+        self.w = np.random.randn(out_dim, in_dim) * np.sqrt(2.0 / in_dim)#weight
+        self.b = np.zeros((out_dim, 1))#bias
         self.dw = np.zeros((out_dim, in_dim))
-        self.db = np.zeros(out_dim)
-
+        self.db = np.zeros((out_dim, 1))
+ 
     def forward(self, input):
-        # BEGIN_LAB
-        # END_LAB
-
+        # print((self.w).shape)
+        # print((input.T).shape)
+        # print(np.dot(self.w, input.T).shape)
+        # print((self.b).shape)
+        mat = np.dot(self.w, input.T) + self.b
+        return mat
+ 
     def backward(self, input, output, d_output):
         batch_size = input.shape[0]
         in_diff = None
         # BEGIN_LAB, compute in_diff/dw/db here
+        self.dw = np.dot(d_output, input) / batch_size
+        self.db = np.sum(d_output, axis=1, keepdims=True) / batch_size
+        in_diff = np.dot(self.w.T, d_output).T
         # END_LAB
         # Normalize dw/db by batch size
         self.dw = self.dw / batch_size
         self.db = self.db / batch_size
         return in_diff
-
+ 
     def update(self):
         self.w = self.w - self.learning_rate * self.dw
         self.b = self.b - self.learning_rate * self.db
-
-
+ 
+ 
 class Softmax(Layer):
     def forward(self, input):
-        row_max = input.max(axis=1).reshape(input.shape[0], 1)
-        x = input - row_max
+        _input = input.T
+        row_max = _input.max(axis=1).reshape(_input.shape[0], 1)
+        x = _input - row_max
         return np.exp(x) / np.sum(np.exp(x), axis=1).reshape(x.shape[0], 1)
-
+ 
     def backward(self, input, output, d_output):
         ''' Directly return the d_output as we show below, the grad is to
             the activation(input) of softmax
         '''
         return d_output
-
-
+ 
 class DNN:
     def __init__(self, in_dim, out_dim, hidden_dim, num_hidden):
         self.layers = []
-        self.layers.append(FullyConnect(in_dim, hidden_dim))
+        self.layers.append(FullyConnect(in_dim, hidden_dim[0]))
         self.layers.append(ReLU())
         for i in range(num_hidden):
-            self.layers.append(FullyConnect(hidden_dim, hidden_dim))
+            self.layers.append(FullyConnect(hidden_dim[i], hidden_dim[i + 1]))
             self.layers.append(ReLU())
-        self.layers.append(FullyConnect(hidden_dim, out_dim))
+        self.layers.append(FullyConnect(hidden_dim[len(hidden_dim) - 1], out_dim))
         self.layers.append(Softmax())
-
+ 
     def set_learning_rate(self, lr):
         for layer in self.layers:
             layer.set_learning_rate(lr)
-
+ 
     def forward(self, input):
         self.forward_buf = []
         out = input
@@ -115,32 +133,35 @@ class DNN:
             self.forward_buf.append(out)
         assert (len(self.forward_buf) == len(self.layers) + 1)
         return out
-
+ 
     def backward(self, grad):
         '''
         Args:
             grad: the grad is to the activation before softmax
         '''
         self.backward_buf = [None] * len(self.layers)
+        # print("1",self.backward_buf.shape)
         self.backward_buf[len(self.layers) - 1] = grad
+        # print("2",self.backward_buf.shape)
         for i in range(len(self.layers) - 2, -1, -1):
+            # print("3",self.backward_buf[i + 1].shape)
             grad = self.layers[i].backward(self.forward_buf[i],
                                            self.forward_buf[i + 1],
-                                           self.backward_buf[i + 1])
+                                           self.backward_buf[i + 1].T)
             self.backward_buf[i] = grad
-
+ 
     def update(self):
         for layer in self.layers:
             layer.update()
-
-
+ 
+ 
 def one_hot(labels, total_label):
     output = np.zeros((labels.shape[0], total_label))
     for i in range(labels.shape[0]):
         output[i][labels[i]] = 1.0
     return output
-
-
+ 
+ 
 def train(dnn):
     utt2feat, utt2target = read_feats_and_targets('train/feats.scp',
                                                   'train/text')
@@ -150,8 +171,9 @@ def train(dnn):
     permute = np.random.permutation(num_samples)
     inputs = inputs[permute]
     labels = labels[permute]
-    num_epochs = 20
+    num_epochs = 100
     batch_size = 100
+    avg_loss = np.zeros(num_epochs)
     for i in range(num_epochs):
         cur = 0
         while cur < num_samples:
@@ -160,7 +182,10 @@ def train(dnn):
             label = labels[cur:end]
             # Step1: forward
             out = dnn.forward(input)
+            print(out)
             one_hot_label = one_hot(label, out.shape[1])
+            # print(one_hot_label.shape)
+            print(out.shape)
             # Step2: Compute cross entropy loss and backward
             loss = -np.sum(np.log(out + 1e-20) * one_hot_label) / out.shape[0]
             # The grad is to activation before softmax
@@ -169,9 +194,11 @@ def train(dnn):
             # Step3: update parameters
             dnn.update()
             print('Epoch {} num_samples {} loss {}'.format(i, cur, loss))
+            avg_loss[i] += loss
             cur += batch_size
-
-
+        avg_loss[i] /= math.ceil(num_samples / batch_size)
+    plot_spectrogram(avg_loss, 'loss.png')
+ 
 def test(dnn):
     utt2feat, utt2target = read_feats_and_targets('test/feats.scp',
                                                   'test/text')
@@ -188,17 +215,16 @@ def test(dnn):
         if t == predict: correct += 1
         print('label: {} predict: {}'.format(t, predict))
     print('Acc: {}'.format(float(correct) / total))
-
-
+ 
+ 
 def main():
     np.random.seed(777)
     # We splice the raw feat with left 5 frames and right 5 frames
     # So the input here is 39 * (5 + 1 + 5) = 429
-    dnn = DNN(429, 11, 128, 1)
-    dnn.set_learning_rate(1e-2)
+    dnn = DNN(429, 11, [170, 200, 150], 2)
+    dnn.set_learning_rate(2e-2)
     train(dnn)
     test(dnn)
-
-
+ 
 if __name__ == '__main__':
     main()
